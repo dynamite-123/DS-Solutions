59d9d71436677e1bb8be0c642520723fdbe9ddfe
dynamite-123 <108745890+dynamite-123@users.noreply.github.com>
2024-10-20 21:19:26 +0530

    added 1c.c

---
+++ 1/1c.c
@@ -0,0 +1,37 @@
+/*c) Create a pointer to an integer array and pass this as parameter to a
+function called Reverse which reverses the array contents using pointer.
+Write different functions called Read (P) to read the array elements
+using pointer P and Display (P) to display the array contents using P
+before and after reverse.*/
+
+#include <stdio.h>
+
+void main(){
+    int n;
+
+    printf("Enter the size of the array:\n");
+    scanf("%d", &n);
+
+    int array[n];
+
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &array[i]);
+    }
+
+    void reverse_array(int *array, int size); //function prototype
+    reverse_array(array, n);
+
+    for (int i = 0; i < n; i++) {
+        printf("%d ", array[i]);
+    }
+}
+
+void reverse_array(int *array, int size_of_array){
+    int temp;
+    for(int i=0; i<size_of_array/2; i++){
+        temp = array[i];
+        array[i] = array[size_of_array - 1 - i];
+        array[size_of_array - 1 - i] = temp;
+    }
+}
