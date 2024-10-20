db1cff2c56fd863e408947b1c75328d1ce47b45d
dynamite-123 <108745890+dynamite-123@users.noreply.github.com>
2024-10-20 19:16:55 +0530

    added 1a.c

---
+++ 1a.c
@@ -0,0 +1,24 @@
+/*a) Create 3 pointers called P1, P2, P3 and an integer variable X where P1
+points to P2, P2 points to P3 and P3 points to X. Demonstrate how to
+read and retrieve the value of X using P1, P2 and P3.*/
+
+#include <stdio.h>
+
+void main() {
+    int x, *P3 = &x, **P2 = &P3, ***P1 = &P2;
+
+    // reading and printing using P3
+    printf("Enter a number:\n");
+    scanf("%d", P3);
+    printf("Entered number is %d\n", *P3);
+
+    // reading and printing using P2
+    printf("Enter a number:\n");
+    scanf("%d", *P2);
+    printf("Entered number is %d\n", **P2);
+
+    // reading and printing using P1
+    printf("Enter a number:\n");
+    scanf("%d", **P1);
+    printf("Entered number is %d\n", ***P1);
+}
